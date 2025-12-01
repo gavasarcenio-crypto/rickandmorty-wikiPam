5528b6c8a559bc41c721c04b1dc1fa20fccd4fcf
gavasarcenio-crypto <ga.vas.arcenio@gmail.com>
2025-12-01 14:40:40 -0300

    F

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,25 +1,24 @@
-import { NavigationContainer } from "@react-navigation/native";
+// src/navigation/AppNavigation.js
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import CharactersListScreen from "../screens/CharactersListScreen";
 import CharacterDetailScreen from "../screens/CharacterDetailScreen";
 
 const Stack = createStackNavigator();
 
-export default function AppNavigator() {
+export default function AppNavigation() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="CharactersList" 
-          component={CharactersListScreen}
-          options={{ title: "Rick and Morty" }}
-        />
-        <Stack.Screen 
-          name="CharacterDetail" 
-          component={CharacterDetailScreen}
-          options={{ title: "Detalhes" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="CharactersList">
+      <Stack.Screen
+        name="CharactersList"
+        component={CharactersListScreen}
+        options={{ title: "Personagens" }}
+      />
+      <Stack.Screen
+        name="CharacterDetail"
+        component={CharacterDetailScreen}
+        options={{ title: "Detalhes" }}
+      />
+    </Stack.Navigator>
   );
 }
