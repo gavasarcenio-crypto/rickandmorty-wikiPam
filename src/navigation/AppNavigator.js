a01fc1a44ef39ca34d0da7779e2759618c3f7477
gavasarcenio-crypto <ga.vas.arcenio@gmail.com>
2025-12-01 16:30:09 -0300

    er

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,24 +1,46 @@
-// src/navigation/AppNavigation.js
-import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import CharactersListScreen from "../screens/CharactersListScreen";
 import CharacterDetailScreen from "../screens/CharacterDetailScreen";
+import portalImg from "../assets/portal.png";
+import logoImg from "../assets/logo.png";
+
 
 const Stack = createStackNavigator();
 
-export default function AppNavigation() {
+export default function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName="CharactersList">
-      <Stack.Screen
-        name="CharactersList"
-        component={CharactersListScreen}
-        options={{ title: "Personagens" }}
-      />
-      <Stack.Screen
-        name="CharacterDetail"
-        component={CharacterDetailScreen}
-        options={{ title: "Detalhes" }}
-      />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator
+  screenOptions={{
+    headerStyle: {
+      backgroundColor: "#1b1e22",
+    },
+    headerTitle: () => (
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Image
+          source={require("../assets/portal.png")}
+          style={{ width: 40, height: 40, marginRight: 8 }}
+        />
+        <Image
+          source={require("../assets/logo.png")}
+          style={{ width: 150, height: 40, resizeMode: "contain" }}
+        />
+      </View>
+    ),
+  }}
+>
+  <Stack.Screen 
+    name="CharactersList" 
+    component={CharactersListScreen}
+  />
+  <Stack.Screen 
+    name="CharacterDetail" 
+    component={CharacterDetailScreen}
+    options={{ title: "Detalhes" }}
+  />
+</Stack.Navigator>
+    </NavigationContainer>
   );
 }
